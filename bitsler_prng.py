837ae9c5e7fd9aee4af68231523a8dece300bb36
hairymanmex <88495774+hairymanmex@users.noreply.github.com>
2021-08-06 12:52:39 -0500

    Bitsler pseudo random generator
    Bitsler pseudo random generator aka provably fair verifier.

---
+++ bitsler_prng.py
@@ -0,0 +1,33 @@
+import hmac
+import hashlib
+import math
+
+
+class bitsler:
+    def __init__(self):
+        self.luckynumber = 0
+
+    def generate(self, nonce, clientseed, serverseed):
+
+        snonce = str(nonce)
+        bnonce = bytes(snonce, 'UTF-8')
+        serverseed = bytes(serverseed, 'UTF-8')
+        clientseed = bytes(clientseed, 'UTF-8')
+        # nonce = bytes(nonce, 'UTF-8')
+        key = bytes(',', 'UTF-8')
+
+        seed = hmac.new(serverseed, clientseed + key + bnonce, hashlib.sha512).hexdigest()
+
+        offset=0
+        fiveset=5
+
+        number = seed[offset:fiveset]
+        number = int(number, 16)
+
+        while number > 999999:
+            number = seed[offset:fiveset]
+            number = int(number, 16)
+            offset+=5
+            fiveset+=5
+        self.luckyNumber = (number % 10000) / 100
+        return self.luckyNumber
